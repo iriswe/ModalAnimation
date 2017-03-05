fbd979f3531ffcd8849a02a3d89659d8dc34800d
ludashen <ludashen@luludeMacBook-Air.local>
2017-03-05 14:34:03 +0800

    Initial Commit

---
+++ PushPopPresentDismissAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushPopPresentDismissAnimation
+//
+//  Created by ludashen on 2017/3/5.
+//  Copyright © 2017年 lulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PushPopPresentDismissAnimationTests/PushPopPresentDismissAnimationTests.m
@@ -0,0 +1,39 @@
+//
+//  PushPopPresentDismissAnimationTests.m
+//  PushPopPresentDismissAnimationTests
+//
+//  Created by ludashen on 2017/3/5.
+//  Copyright © 2017年 lulu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PushPopPresentDismissAnimationTests : XCTestCase
+
+@end
+
+@implementation PushPopPresentDismissAnimationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
